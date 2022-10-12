dfd6ef8ad4d95fa62e2b82756014d29a396e1703
sushi057 <champsuvash@gmail.com>
2022-10-12 10:02:13 +0545

    Dialog Fucntions

---
+++ script.js
@@ -0,0 +1,16 @@
+const dialog = document.querySelector('.add-book');
+const openDialog = document.querySelector('.add-button');
+const closeDialog = document.querySelector('#close-button');
+const addButton = document.querySelector('button');
+
+openDialog.addEventListener('click', () => {
+    dialog.showModal();
+})
+
+closeDialog.addEventListener('click', () => {
+    dialog.close();
+})
+
+addButton.addEventListener('click', () => {
+    dialog.close();
+})
