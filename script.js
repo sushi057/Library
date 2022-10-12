f0dcaea271ae02080360dcb41deae8bbb39c390d
sushi057 <champsuvash@gmail.com>
2022-10-12 10:55:43 +0545

    Constructor function

--- script.js
+++ script.js
@@ -3,15 +3,27 @@ const openDialog = document.querySelector('.add-button');
 const closeDialog = document.querySelector('#close-button');
 const addButton = document.querySelector('button');
 
+// Dialog Events
 openDialog.addEventListener('click', () => {
     dialog.showModal();
 })
 
 closeDialog.addEventListener('click', () => {
     dialog.close();
-    
+
 })
 
 addButton.addEventListener('click', () => {
     dialog.close();
 })
+
+//Database
+
+let myLibrary = [];
+
+function manga(name, author, pages, state){
+    this.name = name
+    this.author = author
+    this.pages = pages
+    this.state = state
+}
